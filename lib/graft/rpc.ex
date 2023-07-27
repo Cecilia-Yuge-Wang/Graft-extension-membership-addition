5c90126449eba90418ea70e22922016175cde765
Cecilia-Yuge-Wang <2809017W@student.gla.ac.uk>
2023-07-27 15:03:33 +0100

    add RPC

--- lib/graft/rpc.ex
+++ lib/graft/rpc.ex
@@ -47,3 +47,51 @@ defmodule Graft.RequestVoteRPCReply do
             # true means candidate received vote
             vote_granted: false
 end
+
+################### Member Change #################
+
+defmodule Graft.MemberChangeRPC do
+  @moduledoc false
+            # server’s term
+  defstruct term: -1,
+            # 0 for join in and 1 for leave
+            change: 0,
+            # name of replying server
+            from: nil
+
+end
+
+defmodule Graft.AppendMemberChangeEntriesRPC do
+  @moduledoc false
+            # leader’s term
+  defstruct term: -1,
+            # so follower can redirect clients
+            leader_name: nil,
+            # index of log entry immediately preceding new ones
+            prev_log_index: -1,
+            # term of prevLogIndex entry
+            prev_log_term: -1,
+            # log entries to store (empty for heartbeat; may send more than one for efficiency)
+            entries: [],
+            # leader’s commit_index
+            leader_commit: -1
+            # number of servers in the cluster
+            server_count: 0,
+            # names of each server in the cluster
+            servers: [],
+
+end
+
+defmodule Graft.AppendMemberChangeEntriesRPCReply do
+  @moduledoc false
+            # current_term, for leader to update itself
+  defstruct term: -1,
+            # true if follower contained entry matching prev_log_index and prev_log_term
+            success: false,
+            # index of last entry in follower's log
+            last_log_index: -1,
+            # term of last entry in follower's log
+            last_log_term: -1,
+            # name of replying server, used to resend new AE with previous entries
+            from: nil
+end
