a3d06ea7022e9a10b454bc28487e5db617a8afc0
Cecilia-Yuge-Wang <2809017W@student.gla.ac.uk>
2023-08-01 00:27:03 +0100

    memberchangeRPC

--- lib/graft/rpc.ex
+++ lib/graft/rpc.ex
@@ -52,46 +52,44 @@ end
 
 defmodule Graft.MemberChangeRPC do
   @moduledoc false
-            # server’s term
-  defstruct term: -1,
-            # 0 for join in and 1 for leave
-            change: 0,
-            # name of replying server
-            from: nil
+            # cluster with all servers, including joining and leaving servers
+  defstruct server_join: [],
+            # cluster with remained servers and joining servers and without leaving servers
+            server_leave: [],
 
 end
 
-defmodule Graft.AppendMemberChangeEntriesRPC do
-  @moduledoc false
-            # leader’s term
-  defstruct term: -1,
-            # so follower can redirect clients
-            leader_name: nil,
-            # index of log entry immediately preceding new ones
-            prev_log_index: -1,
-            # term of prevLogIndex entry
-            prev_log_term: -1,
-            # log entries to store (empty for heartbeat; may send more than one for efficiency)
-            entries: [],
-            # leader’s commit_index
-            leader_commit: -1
-            # number of servers in the cluster
-            server_count: 0,
-            # names of each server in the cluster
-            servers: [],
+# defmodule Graft.AppendMemberChangeEntriesRPC do
+#   @moduledoc false
+#             # leader’s term
+#   defstruct term: -1,
+#             # so follower can redirect clients
+#             leader_name: nil,
+#             # index of log entry immediately preceding new ones
+#             prev_log_index: -1,
+#             # term of prevLogIndex entry
+#             prev_log_term: -1,
+#             # log entries to store (empty for heartbeat; may send more than one for efficiency)
+#             entries: [],
+#             # number of servers in the cluster
+#             server_count: 0,
+#             # names of each server in the cluster
+#             servers: []
 
-end
+# end
 
-defmodule Graft.AppendMemberChangeEntriesRPCReply do
-  @moduledoc false
-            # current_term, for leader to update itself
-  defstruct term: -1,
-            # true if follower contained entry matching prev_log_index and prev_log_term
-            success: false,
-            # index of last entry in follower's log
-            last_log_index: -1,
-            # term of last entry in follower's log
-            last_log_term: -1,
-            # name of replying server, used to resend new AE with previous entries
-            from: nil
-end
+# defmodule Graft.AppendMemberChangeEntriesRPCReply do
+#   @moduledoc false
+#             # current_term, for leader to update itself
+#   defstruct term: -1,
+#             # true if follower contained entry matching prev_log_index and prev_log_term
+#             success: false,
+#             # index of last entry in follower's log
+#             last_log_index: -1,
+#             # term of last entry in follower's log
+#             last_log_term: -1,
+#             # name of replying server, used to resend new AE with previous entries
+#             from: nil
+#             server_count:0
+#             servers: []
+# end
